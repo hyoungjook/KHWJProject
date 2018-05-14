a62bfdd65efd52e929499ba18b15adbba329361b
kimhj1473 <kimhj1473@gmail.com>
2018-05-14 13:40:07 +0900

    기본기능들
    game클래스 기본기능들 약간..

--- game.cpp
+++ game.cpp
@@ -1,3 +1,78 @@
 #include "game.hpp"
 
+void game::renderScene(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+    glutSwapBuffers();
+}
 
+void game::normalKeys(unsigned char key, int x, int y){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::specialKeys(int key, int x, int y){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::idleFunc(){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::reshapeFunc(int w, int h){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
