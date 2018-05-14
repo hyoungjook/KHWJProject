d2a40fc35d58a0cdb2f03aae193f91517afd3902
kimhj1473 <kimhj1473@gmail.com>
2018-05-14 13:50:11 +0900

    연습커밋?
    ??!?

--- game.cpp
+++ game.cpp
@@ -1,4 +1,82 @@
 #include "game.hpp"
 
+<<<<<<< HEAD
 //github.com
+=======
+void game::renderScene(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+    glutSwapBuffers();
+}
+>>>>>>> KHJ_Basic
 
+void game::normalKeys(unsigned char key, int x, int y){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::specialKeys(int key, int x, int y){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::idleFunc(){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
+
+void game::reshapeFunc(int w, int h){
+    
+    switch(gameState){
+        case TITLE:
+            
+            
+            break;
+        case PLAY_1P:
+            
+            
+            break;
+    }
+    
+}
